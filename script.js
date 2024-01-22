e20b40599a9592d13b1a1bf0f97aee7f13fc3d96
ummaar <155968282+eeziioo@users.noreply.github.com>
2024-01-22 15:05:54 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+let ism = prompt('Ismiszni kiriting')
+let familiya = prompt('famliyangizni kiriting')
+let yosh = prompt('yoshingizni kiriting')
+
+let res = `
+Ism: ${ism}
+Familiya: ${familiya}
+Yosh: ${yosh}
+`
+console.log (res)
